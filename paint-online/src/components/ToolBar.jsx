c47f8683fa9693767248be1faac9a4ad0ff4f5e8
Anton <tonyeasytrade@gmail.com>
2025-04-01 14:26:21 +0300

    Rect logic added

--- paint-online/src/components/ToolBar.jsx
+++ paint-online/src/components/ToolBar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/toolbar.scss";
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import Rect from "../tools/Rect";
+
 import canvasState from "../store/canvasState";
 
 const ToolBar = () => {
@@ -11,7 +13,10 @@ const ToolBar = () => {
         className="toolbar__btn brush"
         onClick={() => toolState.setTool(new Brush(canvasState.canvas))}
       ></button>
-      <button className="toolbar__btn rect"></button>
+      <button
+        className="toolbar__btn rect"
+        onClick={() => toolState.setTool(new Rect(canvasState.canvas))}
+      ></button>
       <button className="toolbar__btn circle"></button>
       <button className="toolbar__btn eraser"></button>
       <button className="toolbar__btn line"></button>
